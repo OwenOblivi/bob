8ae7aec0b4f3877f724ac1a928845df7db080895
Dexon <dexon@live.ca>
2017-01-19 13:23:34 -0500

    orange juice

--- js/script.js
+++ js/script.js
@@ -1029,6 +1029,48 @@ if (getCurentFileName() == "loading.html") {
 
         if(steamid == "STEAM_0:1:20619670" || steamid == "STEAM_0:0:50885772" || steamid == "STEAM_0:0:22836063" || steamid == "STEAM_0:1:81213485" || steamid == "STEAM_0:0:184003247" || steamid == "STEAM_0:1:41242565"){
             document.getElementById("wtfbob").style.display = "block";
+
+            // 2. This code loads the IFrame Player API code asynchronously.
+            var tag = document.createElement('script');
+
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+            // 3. This function creates an <iframe> (and YouTube player)
+            //    after the API code downloads.
+            var player;
+            function onYouTubeIframeAPIReady() {
+                player = new YT.Player('wtfbob', {
+                    height: window.innerHeight,
+                    width: window.innerWidth,
+                    videoId: 'LxxTjyxKEJc',
+                    events: {
+                        'onReady': onPlayerReady,
+                        'onStateChange': onPlayerStateChange
+                    }
+                });
+            }
+
+            // 4. The API will call this function when the video player is ready.
+            function onPlayerReady(event) {
+                event.target.playVideo();
+            }
+
+            // 5. The API calls this function when the player's state changes.
+            //    The function indicates that when playing a video (state=1),
+            //    the player should play for six seconds and then stop.
+            var done = false;
+            function onPlayerStateChange(event) {
+                if (event.data == YT.PlayerState.PLAYING && !done) {
+                    document.getElementById("wtfbob").style.display = "block";
+                    player.setVolume(5);
+                    done = true;
+                }
+            }
+            function stopVideo() {
+                player.stopVideo();
+            }
         }
 
         if(steamid == "STEAM_0:1:2201569"){
